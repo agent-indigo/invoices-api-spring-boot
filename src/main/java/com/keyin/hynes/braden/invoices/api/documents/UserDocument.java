09d6a3b1f3169d7980470acb1b7ec63810279f29
agent-indigo <bradenhynes@gmail.com>
2025-04-13 14:08:48 -0230

    Required username to be unique

--- src/main/java/com/keyin/hynes/braden/invoices/api/documents/UserDocument.java
+++ src/main/java/com/keyin/hynes/braden/invoices/api/documents/UserDocument.java
@@ -1,5 +1,6 @@
 package com.keyin.hynes.braden.invoices.api.documents;
 import java.util.List;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,6 +8,7 @@ import lombok.Data;
 @Data
 @Document(collection = "users")
 public final class UserDocument implements UserDetails {
+  @Indexed(unique = true)
   private String username;
   private String password;
   private List<GrantedAuthority> authorities;
