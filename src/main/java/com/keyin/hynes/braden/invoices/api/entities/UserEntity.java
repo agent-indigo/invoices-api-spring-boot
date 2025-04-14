29e10873b2ed06fbcdae026cb53573daf4a51d65
agent-indigo <bradenhynes@gmail.com>
2025-04-14 16:22:01 -0230

    Cleanup

--- src/main/java/com/keyin/hynes/braden/invoices/api/entities/UserEntity.java
+++ src/main/java/com/keyin/hynes/braden/invoices/api/entities/UserEntity.java
@@ -9,11 +9,9 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @Entity
