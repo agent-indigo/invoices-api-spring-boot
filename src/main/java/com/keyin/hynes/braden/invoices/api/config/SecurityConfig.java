558ee284287d89c2b80a2504fe8f136c091e2a67
agent-indigo <bradenhynes@gmail.com>
2025-04-16 18:09:30 -0230

    Configured role hierarchy

--- src/main/java/com/keyin/hynes/braden/invoices/api/config/SecurityConfig.java
+++ src/main/java/com/keyin/hynes/braden/invoices/api/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.keyin.hynes.braden.invoices.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import com.keyin.hynes.braden.invoices.api.services.UserService;
 @Configuration
 @EnableWebSecurity
@@ -15,6 +18,8 @@ public class SecurityConfig {
   @Autowired
   private UserService userDetailsService = new UserService();
   private DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+  private DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+  private DefaultMethodSecurityExpressionHandler methodSecurityExpressionHandler = new DefaultMethodSecurityExpressionHandler();
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity chain) throws Exception {
     chain.authenticationProvider(authenticationProvider());
@@ -38,5 +43,19 @@ public class SecurityConfig {
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(12);
+  }
+  @Bean
+  public RoleHierarchyImpl roleHierarchy() {
+    return RoleHierarchyImpl.fromHierarchy("ROLE_ROOT > ROLE_USER");
+  }
+  @Bean
+  public DefaultWebSecurityExpressionHandler expressionHandler() {
+    expressionHandler.setRoleHierarchy(roleHierarchy());
+    return expressionHandler;
+  }
+  @Bean
+  public DefaultMethodSecurityExpressionHandler methodSecurityExpressionHandler() {
+    methodSecurityExpressionHandler.setRoleHierarchy(roleHierarchy());
+    return methodSecurityExpressionHandler;
   }
 }
